adbeef51ddecd7df1dbaf18e1bc71fd2b03af72b
Виталий Матюшенко <kuyask@ya.ru>
2024-03-12 09:36:03 +0300

    Подчистил семинар 3

--- Seminar03/src/Main.java
+++ Seminar03/src/Main.java
@@ -47,53 +47,5 @@ public class Main {
         System.out.println("_______________Сортировка по весу через Лямбду");
         Collections.sort(medList,(comp1, comp2)-> (int)(comp1.getWeight()- comp2.getWeight())); // Сортировка по весу через Лямбду
         System.out.println(medList);
-
-        //Код со второй домашки
-//        VeterinaryClinic clinic = new VeterinaryClinic();
-//        //Добавление животных
-//        Animal dog = new Dog("Vasya", 7, LocalDate.now(), new Owner());
-//        Jellyfish jellyfish = new Jellyfish("Пузырек", 1, LocalDate.now(), new Owner());
-//        clinic.addAnimal(dog);
-//        clinic.addAnimal(jellyfish);
-//        clinic.addAnimal(new Bat("Чуча", 1, LocalDate.now(), new Owner()));
-//        System.out.println(clinic.getSwimable());//получение списка плавающих животных
-//        System.out.println(clinic.getAnimals()); //получение всех животных
-//
-//        //Добавление работников
-//        Doctor doc = new Doctor("Доктор1", (short) 32, LocalDate.of(2023, 12, 12));
-//        clinic.addWorker(doc);
-//        System.out.println(clinic.getDoctors());
-//
-//        //Диагноз
-//        Zebra zebra = new Zebra("Полосатик", 200, LocalDate.now(), new Owner("Чебурашка"));
-//        doc.diagnose(zebra);
-
-
-//Код с семинара
-//        Animal dog = new Dog("Vasya", 7, LocalDate.now(), new Owner()); //Правильное создание экземпляра класса Animal
-//        //System.out.println(dog);
-//        Jellyfish jellyfish = new Jellyfish("Пузырек", 1, LocalDate.now(), new Owner());
-//        jellyfish.sound();
-//        dog.eat();
-//
-//        List<Animal>flyables = new ArrayList<>();
-//        flyables.add(new Duck("Кряка", 1, LocalDate.now(), new Owner()));
-//        flyables.add(new Bat("Мышонок", 1, LocalDate.now(), new Owner()));
-//        flyables.add(dog);
-//        //System.out.println(flyables);
-//
-//        for (Animal animal: flyables){
-//            if (animal instanceof Flyable){
-//                ((Flyable) animal).fly();// Приведение к типу (операция каст)
-//            }
-//        }
-        //Анонимный класс при таком вызове(через new) нужно реализовывать все абстрактные методы
-        // Посмотреть сколько методов нужно реализовывать через List list = new List(). Правильное создание на 15 строке
-//        Animal dog = new Animal() {
-//           @Override
-//           public void eat() {
-//
-//           }
-//       };
     }
 }
